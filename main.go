7f971e4c2eceae9f5562462ff73aa58aa54a6984
Steve Wishstar <steve@medchain.us>
2018-06-11 14:14:39 -0800

    Initial commit from https://github.com/chainHero/heroes-service/tree/v1.0.5 (Apache 2.0 license).

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"github.com/chainHero/heroes-service/blockchain"
+	"github.com/chainHero/heroes-service/web"
+	"github.com/chainHero/heroes-service/web/controllers"
+	"os"
+)
+
+func main() {
+	// Definition of the Fabric SDK properties
+	fSetup := blockchain.FabricSetup{
+		// Channel parameters
+		ChannelID:     "chainhero",
+		ChannelConfig: os.Getenv("GOPATH") + "/src/github.com/chainHero/heroes-service/fixtures/artifacts/chainhero.channel.tx",
+
+		// Chaincode parameters
+		ChainCodeID:     "heroes-service",
+		ChaincodeGoPath: os.Getenv("GOPATH"),
+		ChaincodePath:   "github.com/chainHero/heroes-service/chaincode/",
+		OrgAdmin:        "Admin",
+		OrgName:         "Org1",
+		ConfigFile:      "config.yaml",
+
+		// User parameters
+		UserName: "User1",
+	}
+
+	// Initialization of the Fabric SDK from the previously set properties
+	err := fSetup.Initialize()
+	if err != nil {
+		fmt.Printf("Unable to initialize the Fabric SDK: %v\n", err)
+	}
+
+	// Install and instantiate the chaincode
+	err = fSetup.InstallAndInstantiateCC()
+	if err != nil {
+		fmt.Printf("Unable to install and instantiate the chaincode: %v\n", err)
+	}
+
+	// Launch the web application listening
+	app := &controllers.Application{
+		Fabric: &fSetup,
+	}
+	web.Serve(app)
+}
